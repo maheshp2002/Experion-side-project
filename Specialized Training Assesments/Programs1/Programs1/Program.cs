4f0ab5f435cec8369edac9175fb0b35d52a8a0e9
Mahesh P <pmahesh072002@gmail.com>
2023-01-14 20:44:32 +0530

    new

--- Specialized Training Assesments/Programs1/Programs1/Program.cs
+++ Specialized Training Assesments/Programs1/Programs1/Program.cs
@@ -1,20 +1,24 @@
 ﻿String[] namesArray = new string[10];
 
+
+// sorting
+
+// input array
+int number = 1;
 for(int i = 0; i < 10; i++) {
-    int number = 1;
     Console.WriteLine("Enter name\t" + number);
     namesArray[i] = Console.ReadLine();
     number ++;
 }
 
-//for (int i = 0; i < namesArray.Length; i++)
-//{
-//    for (int j = 0; j < namesArray.Length; j++)
-//    {
-//        if (namesArray[i] < namesArray[j])
-//        {
+// output sorted array
+Console.WriteLine("After sort\n");
+Array.Sort(namesArray);
 
-//        }
-//    }
-//}
+foreach(string g in namesArray)
+{
+    Console.WriteLine(g);
+}
 
+
+// 
